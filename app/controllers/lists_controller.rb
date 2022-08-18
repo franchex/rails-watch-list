88c46ec792580e5d440acdf8035b8be7417b273f
Francisco Sotelo <franciscosotelov@gmail.com>
2022-08-18 16:56:44 -0500

    simple form de lists added

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,14 @@
+class ListsController < ApplicationController
+
+ def index
+  @lists = List.all
+ end
+
+ def show
+  @lists = List.find(params[:id])
+ end
+
+ def new
+  @lists = Lits.new
+ end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root to: "pages#home"
+  root 'lists#index'
+  get 'lists/:id', to: 'lists#show'
+  get 'lists/new', to: 'lists#new'
+  get 'movies', to: 'movies#index'
+
+  #get 'lists' to: 'lists#index
+
   # Defines the root path route ("/")
   # root "articles#index"
 end
